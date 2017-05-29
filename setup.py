54d72d7801033a2071efac4901ce5e7b7d61784c
Kartik Thakore <kthakore@alumni.uwo.ca>
2017-05-29 13:03:30 -0700

    Added setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='MusiganPyAudioAnalysis',
     version='0.1',
-    packages=['musigan-py-audio-analysis',],
+    ackages=['musigan-py-audio-analysis',],
     license='Apache License 2.0',
     long_description=open('README.txt').read(),
 )
